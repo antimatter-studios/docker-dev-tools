2ea766c940f3991705bb2775d89a25e434499d98
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-03-06 17:58:18 +0100

    allow this project variable to be used from the exception object

--- src/Exceptions/Project/ProjectNotFoundException.php
+++ src/Exceptions/Project/ProjectNotFoundException.php
@@ -3,8 +3,18 @@ namespace DDT\Exceptions\Project;
 
 class ProjectNotFoundException extends \Exception
 {
+    /** @var string */
+    private $project;
+
     public function __construct(string $project, $code = 0, \Throwable $previous = null)
     {
         parent::__construct("Could not find project '$project' with given parameters", $code, $previous);
+
+        $this->project = $project;
+    }
+
+    public function getProject(): string
+    {
+        return $this->project;
     }
 };
