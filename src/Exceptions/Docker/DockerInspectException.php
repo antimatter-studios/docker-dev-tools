fb64f3d6b604021647b891183ed31e09508abc89
Christopher Thomas <chris.thomas@antimatter-studios.com>
2022-03-19 19:56:31 +0100

    only get the message from the previous exception when one is provided

--- src/Exceptions/Docker/DockerInspectException.php
+++ src/Exceptions/Docker/DockerInspectException.php
@@ -6,7 +6,7 @@ class DockerInspectException extends \Exception
 {
     public function __construct(string $type, string $name, int $code = 0, \Throwable $previous = null)
     {
-        $message = trim($previous->getMessage());
+        $message = $previous ? trim($previous->getMessage()) : 'no message given';
         parent::__construct("Docker could not find the '$type' resource named '$name' to inspect ($message)", $code, $previous);
     }
 }
