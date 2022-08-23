3542c7abb9dd12bc62d21a3ad6a2c0eba472cde0
Christopher Thomas <chris.thomas@antimatter-studios.com>
2022-08-24 00:15:52 +0200

    update to refactor against the repository model object

--- src/Services/GitService.php
+++ src/Services/GitService.php
@@ -3,8 +3,8 @@ namespace DDT\Services;
 
 use DDT\CLI;
 use DDT\Exceptions\Filesystem\DirectoryExistsException;
-use DDT\Exceptions\Filesystem\DirectoryNotExistException;
-use DDT\Exceptions\Git\GitRepositoryNotFoundException;
+use DDT\Exceptions\Git\GitRepositoryCloneException;
+use DDT\Model\Git\GitRepositoryModel;
 use DDT\Model\Vcs\VcsModel;
 use InvalidArgumentException;
 
@@ -18,17 +18,15 @@ class GitService
 		$this->cli = $cli;
 	}
 
+	// Eventually remove this method completely
 	static public function fromPath(string $path): VcsModel
     {
 		$service = container(self::class);
+		$repo = $service->getRepository($path);
+		$url = $repo->remote();
+		$branch = $repo->branch();
 
-		if($service->isRepository($path)){
-			$url = $service->remote($path);
-			$branch = $service->branch($path);
-			return container(VcsModel::class, ['url' => $url, 'branch' => $branch]);
-		}
-
-		throw new GitRepositoryNotFoundException($path);
+		return container(VcsModel::class, ['url' => $url, 'branch' => $branch]);
     }
 
 	public function exists(string $url): bool
@@ -37,20 +35,18 @@ class GitService
 		return $this->cli->getExitCode() === 0;	
 	}
 
-	public function isRepository(string $path): bool
+	public function getRepository(string $path): GitRepositoryModel
 	{
-		$this->cli->exec("git -C $path remote -v");
-
-		return $this->cli->getExitCode() === 0;
+		return container(GitRepositoryModel::class, ['path' => $path]);
 	}
 
 	/**
 	 * @param string $url
 	 * @param string $path
-	 * @return bool
+	 * @return GitRepositoryModel
 	 * @throws DirectoryExistsException
 	 */
-	public function clone(string $url, string $path): bool
+	public function clone(string $url, string $path): GitRepositoryModel
 	{
 		if(is_dir($path)){
 			throw new DirectoryExistsException($path);
@@ -60,60 +56,10 @@ class GitService
 			throw new InvalidArgumentException("The url '$url' is not a valid git repository");
 		}
 
-		return $this->cli->passthru("git clone $url $path") === 0;
-	}
-
-	/**
-	 * @param string $path
-	 * @return bool
-	 * @throws DirectoryNotExistException
-	 */
-	public function pull(string $path): bool
-	{
-		if(!is_dir($path)){
-			throw new DirectoryNotExistException($path);
+		if($this->cli->passthru("git clone $url $path") !== 0){
+			throw new GitRepositoryCloneException($url, $path);
 		}
 
-		return $this->cli->passthru("git -C $path pull") === 0;
-	}
-
-	public function push(string $path): bool
-	{
-		if(!is_dir($path)){
-			throw new DirectoryNotExistException($path);
-		}
-
-		return $this->cli->passthru("git -C $path push") === 0;
-	}
-
-	public function status(string $path): string
-	{
-		$output = $this->cli->exec("git -C $path status -s");
-		$output = trim($output);
-
-		return $output;
-	}
-
-	public function branch(string $path): string
-	{
-		return $this->cli->exec("git -C $path rev-parse --abbrev-ref HEAD");
-	}
-
-	public function remote(string $path, string $name='origin'): string
-	{
-		if($this->isRepository($path)){
-			return $this->cli->exec("git -C $path remote get-url $name");
-		}
-
-		throw new GitRepositoryNotFoundException($path);
-	}
-
-	public function fetch(string $path, bool $prune=false): bool
-	{
-		$prune = $prune ? "-p" : "";
-
-		$this->cli->exec("git -C $path fetch $prune");
-
-		return $this->cli->getExitCode() === 0;
+		return container(GitRepositoryModel::class, ['path' => $path]);
 	}
 }
