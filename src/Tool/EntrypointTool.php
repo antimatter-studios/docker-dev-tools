bb2977cec1f942f39e356ec5315e660205b0962b
Christopher Thomas <chris.thomas@antimatter-studios.com>
2022-10-27 14:59:38 +0200

    Don't update when running the setup tool

--- src/Tool/EntrypointTool.php
+++ src/Tool/EntrypointTool.php
@@ -72,8 +72,9 @@ class EntrypointTool extends Tool implements ToolRegistryInterface
         }
 
         // Special case for the auto-updater
-        // Before handling the request, check to see if the timeout passes and self update
-        if ($toolName !== 'self-update') {
+        // Don't update when you're running the setup tool
+        // Don't update when you're running the self-update tool
+        if (!in_array($toolName, ['setup', 'self-update'])) {
             $this->getTool('self-update')->auto();
         }
 
