e43d87ac5b61520f04535f664981d4d8123051da
Christopher Thomas <chris.thomas@antimatter-studios.com>
2022-12-16 01:41:39 +0100

    alllow custom channels to filter in or out certain text strings

--- src/CLI/Output/CustomChannel.php
+++ src/CLI/Output/CustomChannel.php
@@ -3,6 +3,9 @@
 namespace DDT\CLI\Output;
 
 class CustomChannel extends HistoryChannel {
+    private $in = [];
+    private $out = [];
+
     public function __construct(?string $name = 'custom', bool $record=true)
     {
         $this->setName($name);
@@ -18,6 +21,14 @@ class CustomChannel extends HistoryChannel {
             // First, render the passed data to a string using various ways to convert objects, arrays, scalars, etc
             $string = $this->renderString($string, $params);
 
+            // Pass the string through the filters
+            $string = $this->filterString($string);
+
+            // If the string is empty now, it didn't pass the filters, 
+            // or the string after rendering and filtering is empty
+            // in this case, we have nothing to process further, so quit early
+            if(empty($string)) return $string;
+
             // If any, render a prefix to start each string
             $string = $this->renderPrefix($string);
 
@@ -27,6 +38,29 @@ class CustomChannel extends HistoryChannel {
             // Send out the string to every listener attached
             $string = $this->sendToListeners($string);
         }
+
+        return $string;
+    }
+
+    public function filterIn(string $string): void
+    {
+        $this->in[] = $string;
+    }
+
+    public function filterOut(string $string): void 
+    {
+        $this->out[] = $string;
+    }
+
+    private function filterString(string $string): string
+    {
+        $string = array_reduce($this->in, function($a, $i){
+            return !empty($a) && strpos($a, $i) !== false ? $a : '';
+        }, $string);
+
+        $string = array_reduce($this->out, function($a, $i) {
+            return !empty($a) && strpos($a, $i) === false ? $a : '';
+        }, $string);
 
         return $string;
     }
